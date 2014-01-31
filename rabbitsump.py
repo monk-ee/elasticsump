f83dde098a4604d40853913a80f05618a9b313d1
Lyndon Swan <magic.monkee.magic@gmail.com>
2014-01-31 14:48:46 +1000

    added rabbitsump

--- rabbitsump.py
+++ rabbitsump.py
@@ -3,7 +3,7 @@ __author__ = 'monk-ee'
 
 """This module hits an elasticsearch index - flattens the data and puts up on s3 ready for redshiftiness.
 """
-from pyes import *
+import pika
 import csv
 import logging
 import yaml
@@ -11,6 +11,11 @@ import boto
 import time
 import os
 
+#event mapping
+event_mapping = ['app_key',	'device_id', 'event_label', 'event_type', 'phash',	'timestamp']
+
+#session
+session_mapping = ['ab_split_id', 'app_key', 'app_name', 'app_version',	'campaign_id',	'country',	'developer_key', 'device_id', 'eventname',	'eventtype', 'language', 'lv', 'manufacturer',	'os_name', 'os_version', 'phash', 'platform', 'puid', 'remoteaddress', 'session_length', 'template_id',	'timestamp', 'timezone', 'tracking_id',	'type',	'version']
 
 
 
@@ -30,24 +35,28 @@ class Sump:
             exit("Failed Configuration")
         logging.basicConfig(filename=self.config['general']['logfile'],level=logging.INFO)
         try:
-            self.conn = ES(self.config['servers'], timeout=self.config['es']['timeout'])
+            self.conn = pika.BlockingConnection(pika.ConnectionParameters(self.config['rabbitmq']['server']))
         except:
             #done again
-            exit("Failed to connect to ElasticSearch")
+            exit("Failed to connect to RabbitMQ")
 
     def search(self):
-        q = MatchAllQuery()
+        i = 0
+        while i < 100000:
+            channel = self.conn.channel()
+            method_frame, header_frame, body = channel.basic_get(self.config['rabbitmq']['channel'])
+            if method_frame:
+                print method_frame, header_frame, body
+                self.parseRow(body)
+                channel.basic_ack(method_frame.delivery_tag)
+            else:
+                print 'No message returned'
 
-        result = self.conn.search(query=q, indices=[self.config['es']['index']])
-
-        for r in result:
-            #print r this will probably need to be a buffer
-            self.parseRow(r)
+            i = i + 1
 
     def parseRow(self,row):
         #step 1 flatten the json to a flat dict
         flatdict = self.flatten(row)
-
         #step 2 grab all the keys for the header row
         csvrow = []
         header = []
@@ -56,8 +65,6 @@ class Sump:
             header.append(key)
         #hash it
         header_hash = hash(frozenset(header))
-        #sort header
-        header = sorted(header)
         #now we will have to check if that header exists
         try:
             y = self.headers_nest[header_hash]
@@ -67,19 +74,19 @@ class Sump:
             self.csvme(header_hash,header)
         #output values into the appropriate nest
         # need to convert timestamps to sql friendly format
-        #for key,value in flatdict.iteritems():
-        #     try:
-        #         #this situation: UnicodeEncodeError: 'ascii' codec can't encode characters in position 0-11: ordinal not in range(128)
-        #        value.encode('ascii', 'ignore')
-        #    except:
-        #        pass
-        #    if key == self.config['es']['date_column']:
-        #        value = time.strftime(self.config['es']['date_format'], time.localtime(value / 1e3))
-        #    csvrow.append(value)
-        #self.csvme(header_hash,csvrow)
+        for key,value in flatdict.iteritems():
+            try:
+                #this situation: UnicodeEncodeError: 'ascii' codec can't encode characters in position 0-11: ordinal not in range(128)
+                value.encode('ascii', 'ignore')
+            except:
+                pass
+            if key == self.config['es']['date_column']:
+                value = time.strftime(self.config['es']['date_format'], time.localtime(value / 1e3))
+            csvrow.append(value)
+        self.csvme(header_hash,csvrow)
 
     def csvme(self,header_hash,row_list):
-        writer = csv.writer(open('csv/headers.csv', 'a') , delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer = csv.writer(open('csv/'+str(header_hash)+'.csv', 'a') , delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         try:
             writer.writerow(row_list)
         except:
@@ -145,5 +152,5 @@ class Sump:
 
 
 if __name__ == "__main__":
-    elasticsump = Sump()
-    elasticsump.search()
+    rabbitsump = Sump()
+    rabbitsump.search()
